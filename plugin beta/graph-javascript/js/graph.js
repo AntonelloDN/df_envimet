89432ed79f5dacebd1854888dfc70b2b75e01526
dna <antonellodinunzio@gmail.com>
2020-05-30 17:54:47 +0200

    Create graph.js

---
+++ plugin beta/graph-javascript/js/graph.js
@@ -0,0 +1,16 @@
+options["theme"] = {"palette": "palette6"};
+options["stroke"] = {"width": 1.5};
+options["toolbar"] = {
+            "show": true,
+        "tools": {
+          "download": true,
+          "selection": true,
+          "zoom": true,
+          "zoomin": true,
+          "zoomout": true,
+          "pan": true,
+          "reset": true | '<img src="/static/icons/reset.png" width="20">',
+          "customIcons": []
+        },
+        "autoSelected": 'zoom'
+};
